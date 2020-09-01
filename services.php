786c0ca35af4b25a00d43b113307c3a39c078d59
Juan Espinosa <arq.jorrin@gmail.com>
2020-09-01 21:46:03 +1000

    Add services second section

--- services.php
+++ services.php
@@ -22,6 +22,86 @@
 
 		<section id="servicesSecondSection">
 			<h1 class="servicesHeading">Services</h1>
+			<div class="service">
+				<img src="./icons/icon_service_engine.png" alt="Engine Service Icon" />
+				<h3>Engine Service</h3>
+				<ul>
+					<li>Oil and Filter Change</li>
+					<li>Engine Diagnosis</li>
+					<li>Diesel Service</li>
+					<li>Hybrid Vehicles</li>
+				</ul>
+			</div>
+			<div class="service">
+				<img src="./icons/icon_service_battery.png" alt="Electronic Systems Icon" />
+				<h3>Electronic Systems</h3>
+				<ul>
+					<li>Battery Service</li>
+					<li>Electronic Diagnosis</li>
+					<li>Start/Stop System</li>
+				</ul>
+			</div>
+			<div class="service">
+				<img src="./icons/icon_service_tyre.png" alt="Tyre Service Icon" />
+				<h3>Tyre Service</h3>
+				<ul>
+					<li>Balancing and Wheel Change</li>
+					<li>Servicing of Inflation Pressure Monitors</li>
+					<li>Top Quality Tyres from Leading Manufacturers</li>
+				</ul>
+			</div>
+			<div class="service">
+				<img src="./icons/icon_service_brake.png" alt="Brake Servicing Icon" />
+				<h3>Brake Servicing</h3>
+				<ul>
+					<li>Repair and Maintenance</li>
+					<li>Vehicle Safety Systems Diagnosis</li>
+				</ul>
+			</div>
+			<div class="service">
+				<img src="./icons/icon_service_inspection.png" alt="Inspection and Checks Icon" />
+				<h3>Inspection and Checks</h3>
+				<ul>
+					<li>Inspection</li>
+					<li>Holiday Check</li>
+					<li>Vehicle Check</li>
+					<li>10 Point Vehicle Safety Check</li>
+					<li>Blue/Pink Slip</li>
+				</ul>
+			</div>
+			<div class="service">
+				<img src="./icons/icon_service_mechanics.png" alt="Mechanical Repairs Icon" />
+				<h3>Mechanical Repairs</h3>
+				<ul>
+					<li>Genuine Spare Parts (Subject to Availability)</li>
+					<li>The Latest Test Equipment for Mechanical Vehicle Components</li>
+					<li>Qualified Personnel with Access to Technical Data</li>
+				</ul>
+			</div>
+			<div class="service">
+				<img src="./icons/icon_service_glass.png" alt="Glass Repair Icon" />
+				<h3>Glass Repair</h3>
+				<ul>
+					<li>Professional Replacement of the Entire Windshield</li>
+				</ul>
+			</div>
+			<div class="service">
+				<img src="./icons/icon_service_light.png" alt="Globe Inspection Icon" />
+				<h3>Globe Inspection</h3>
+				<ul>
+					<li>Replacement and Repair of Individual Components</li>
+					<li>Complimentary Light Test Using the Latest Test Equipment</li>
+					<li>Top Quality Automotive Bulbs</li>
+				</ul>
+			</div>
+			<div class="service">
+				<img src="./icons/icon_service_climate.png" alt="Climate Control Service Icon" />
+				<h3>Climate Control Service</h3>
+				<ul>
+					<li>A/C System Services and Repairs</li>
+					<li>Lic. No. AU43798</li>
+				</ul>
+			</div>
 		</section>
 	</main>
 
