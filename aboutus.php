1526c155e4afc5c9fd04c148d629e14faccf6c15
Juan Espinosa <arq.jorrin@gmail.com>
2020-09-03 14:15:28 +1000

    Add About Us page and its first section

---
+++ aboutus.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title>About Us | Speedy Auto Centre</title>
+	<?php
+		require "header-footer-php/head.php";
+	?>
+</head>
+
+<body>
+    <header id="aboutUsHeader" class="whiteHeader hideWhiteNav">
+		<?php
+			require "header-footer-php/header.php";
+		?>
+	</header>
+
+	<main>
+  <section id="aboutUsFirstSection">
+			<div class="aboutUsBackground"></div>
+    </section>
+
+    <section id="aboutUsSecondSection">
+      <h1 class="aboutUsHeading">About Us</h1>
+
+    </section>
+
+  </main>
+
+	<footer id="websiteFooter">
+		<?php
+			require "header-footer-php/footer.php";
+		?>
+	</footer>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script src="./js/main.js" type="text/javascript"></script>
+</body>
+</html>
