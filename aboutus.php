9be4818cecd50671cf01419423e65363ae6a8a4e
Juan Espinosa <arq.jorrin@gmail.com>
2020-09-18 23:09:57 +1000

    Style About Us page

--- aboutus.php
+++ aboutus.php
@@ -22,18 +22,19 @@
 
     <section id="aboutUsSecondSection">
       <h1 class="aboutUsHeading">About Us</h1>
-      <h3>Who we are</h3>
-      <p>
+      <div class="verticalImage2"></div>
+      <h3 class="aboutUsSubheading aboutUsSubheadingOne">Who we are</h3>
+      <p class="aboutUsParagraph">
         For over 10 years, <strong>Speedy Auto Centre</strong> has been proud to bring Australians honest, reliable and afforable solutions for their auto repair needs.
         Our success comes from our emphasis on friendly service, with a goal of building long lasting relationships with our valued customers.
         This personal touch, combined with the unmatched technical knowledge of our <strong>certified team</strong> is what makes Speedy the best choice for auto repairs.
-      </p>
-      <p>
+        <br/>
+        <br/>
         Our staff is always willing to go above and beyond; whether you're in for a basic oil change or a major overhaul, they'll do everything they can to ensure you're completely satisfied before leaving our repair facility.
         We aim to bring you auto repair that is as rich in a good customer experience as it is in quality.
       </p>
-      <h3>A full service auto repair shop</h3>
-      <p>
+      <h3 class="aboutUsSubheading">A full service auto repair shop</h3>
+      <p class="aboutUsParagraph aboutUsParagraphTwo">
         <strong>Speedy Auto Centre</strong> is an auto repair shop that offers all forms of repair and maintenance for your vehicle.
         We bring you the highest standard of service in brakes, tyres, wheel balancing, batteries, windscreen, suspension, oil, and mucho more.
         Our team consists of highly trained and certified technicians that have a wide range of knowledge in all fields of vehicle service and maintenance, and they'll take the time to speak with you about the solutions our auto repair shop offers, and what's best for your car.
